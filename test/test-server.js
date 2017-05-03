27900161ebd21e947c1428ce8049a38995f961f5
meron93 <meronyemane18@gmail.com>
2017-05-03 13:48:29 -0700

    added .travis.yml file

---
+++ test/test-server.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../server.js');
+
+const app = server.app;
+//const storage = server.storage;
+
+const should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('index.html page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      });
+  });
+});
