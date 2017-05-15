eb28ecde99aa19c67c3ed1d9e1afde198269638c
meron93 <meronyemane18@gmail.com>
2017-05-14 18:40:08 -0700

    implemented static sign-up page and event listener for sign-up link on login page

--- public/app.js
+++ public/app.js
@@ -2,5 +2,9 @@ $(document).ready(function() {
   $("#signup, #homepage, #progresspage").hide();
 
 
+});
 
+$(document).on("click", "#great", function() {
+  $("#login").hide();
+  $("#signup").show();
 });
