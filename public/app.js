a30e9a75a8740fefa434924e2205a58584f43b16
meron93 <meronyemane18@gmail.com>
2017-05-27 16:22:00 -0700

    progress page: added navbar

--- public/app.js
+++ public/app.js
@@ -2,9 +2,35 @@ $(document).ready(function() {
   $("#signup, #homepage, #progresspage").hide();
 
 
-});
 
-$(document).on("click", "#great", function() {
-  $("#login").hide();
-  $("#signup").show();
+  $(document).on("click", "#sign-up-button", function() {
+    $("#login").hide();
+    $("#signup").show();
+  });
+
+  $(document).on("click", "#homepage-button", function() {
+    $("#login").hide();
+    $("#homepage").show();
+  });
+
+  $(document).on("click", "#login-button", function() {
+    $("#signup").hide();
+    $("#login").show();
+  });
+
+  $(document).on("click", "#progress-button", function() {
+    $("#homepage").hide();
+    $("#progresspage").show();
+  });
+
+  $(document).on("click", "#homenav-button", function() {
+    $("#progresspage").hide();
+    $("#homepage").show();
+  });
+
+  $(document).on("click", "#logout-button", function() {
+    $("#progresspage").hide();
+    $("#homepage").hide();
+    $("#login").show();
+  });
 });
