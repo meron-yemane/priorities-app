b8da9443f803093188c8a98d62286974999bae79
meron93 <meronyemane18@gmail.com>
2017-06-10 19:40:04 -0700

    Schema created in models.js

---
+++ models.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const PrioritySchema = new Schema({
+  id: {type: Number,required: true}, 
+  goal: {type: String, required: true}, 
+  completed: {type: Boolean, required: true},
+  date_committed: type: Date, required: true}
+});
+
+const Priorities = mongoose.model('Priorities', PrioritySchema);
+
+module.exports = Priorities
